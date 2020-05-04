419ad7a100fb66ff1e99d7803a68265a578052c7
Sebastian Zarski <seba.zarski@gmail.com>
2020-05-04 22:20:14 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+
+    console.log('Welcome to Brownells Europe');
+    const firstName = 'Seba';
+    const age = '40';
+    console.log(firstName);
+    console.log(age);
+    console.log(`My name is ${firstName} and I'm ${age} years old`);
